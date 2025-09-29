b8dd2a5f6a6cc683cf62f1e14914e19068b2e8a2
ZechariaTomer <zechariatomer@gmail.com>
2025-09-29 21:36:09 +0300

    Setup Django with Docker, Postgres, Redis and market app

---
+++ market/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all().values("id", "name", "price")
+    return JsonResponse(list(products), safe=False)
